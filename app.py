e0efda3b0976a399c443efe1502ec1daa9b60ce6
sadiqshaik-ui <sadiq.shaik@archents.com>
2026-02-13 12:01:59 +0530

    Initial commit - calculator app

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Calculator API is Running 🚀"
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "healthy"}), 200
+
+@app.route("/add", methods=["GET"])
+def add():
+    a = int(request.args.get("a", 0))
+    b = int(request.args.get("b", 0))
+    return jsonify({"result": a + b})
+
+@app.route("/sub", methods=["GET"])
+def sub():
+    a = int(request.args.get("a", 0))
+    b = int(request.args.get("b", 0))
+    return jsonify({"result": a - b})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
